32819fe626405f576469e580065e9fbbbd21ead1
Piyush More <140102487+MorePiyush17@users.noreply.github.com>
2025-06-18 08:13:42 -0700

    Update FunctionsandArray.cs

--- FunctionsandArray.cs
+++ FunctionsandArray.cs
@@ -4,53 +4,52 @@ namespace FunctionsandArray
 {
     class Program
     {
+        // created function to apply 5% discount if the fee > 7000
         static double[] ApplyDiscount(double[] fees)
         {
             double[] finalFees = new double[fees.Length];
-           
+
             for (int i = 0; i < fees.Length; i++)
             {
-            if (fees[i] > 7000)
+                if (fees[i] > 7000)
                 {
-                 finalFees[i] = fees[i] - (fees[i] * 0.05); 
-              }
+                      finalFees[i] = fees[i] - (fees[i] * 0.05);
+                 }
 
-             if (fees[i] <= 7000)
-                  {
-                finalFees[i] = fees[i];
-             }
-         }
-
+                if (fees[i] <= 7000)
+                {
+                    finalFees[i] = fees[i];
+                }
+            }
             return finalFees;
-        }
+       }
 
         static void Main()
         {
-          double[] studentFees = new double[4]; 
-            for (int i = 0; i < 4; i++)
-               {
-                double fee;
+             double[] studentFees = new double[4];
 
-             do
-              {
+         
+            for (int i = 0; i < 4; i++)
+            {
+                double fee = 0;
                 Console.Write("Enter fees for Student " + (i + 1) + " : ");
                 fee = Convert.ToInt32(Console.ReadLine());
 
-                if (fee < 5000 || fee > 10000)                     {
-                Console.WriteLine("Error! Fees must be in range 5000 to 10000");
-                    }
-
-                  } while (fee < 5000 || fee > 10000);
-
+                while (fee < 5000 || fee > 10000)
+                {
+                    Console.WriteLine("Error! Fees must be in range 5000 to 10000");
+                    Console.Write("Enter fees for Student " + (i + 1) + " again: ");
+                    fee = Convert.ToInt32(Console.ReadLine());
+                }  
                 studentFees[i] = fee;
-               }
-         
+            }
             double[] finalFees = ApplyDiscount(studentFees);
 
-            Console.WriteLine("\n Fees with discount are:");
+          //to display fees after discount
+            Console.WriteLine("\nFees with discount are:");
             for (int i = 0; i < finalFees.Length; i++)
             {
-                Console.WriteLine("Student " + (i + 1) + " : " + finalFees[i]); 
+              Console.WriteLine("Student " + (i + 1) + " : " + finalFees[i]);
             }
         }
     }
