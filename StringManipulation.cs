c8eed4cac8b194f150bafc287facb2dcac4e8320
Piyush More <140102487+MorePiyush17@users.noreply.github.com>
2025-06-23 00:14:33 +0530

    Add files via upload

---
+++ StringManipulation.cs
@@ -0,0 +1,20 @@
+﻿namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Sachin, Aurionpro, Mumbai";
+
+            //split of string 
+            string[] parts = input.Split(',');
+
+            //display output
+
+            Console.WriteLine("Developer Name: " + parts[0]);
+            Console.WriteLine("Comapny name is : " + parts[1]);
+            Console.WriteLine("Location is : " + parts[2]);
+
+        }
+    }
+}
