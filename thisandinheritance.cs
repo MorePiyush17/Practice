eba2b7be4da3c0e87dbe9185bb1785336f7a4765
Piyush More <140102487+MorePiyush17@users.noreply.github.com>
2025-06-24 00:17:30 +0530

    Add files via upload

---
+++ thisandinheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace thisandinheritance
+{
+
+    class Shape
+    {
+
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+
+    // derived class using 'this' keyword
+    class Circle : Shape
+    {
+        private const double pi = 3.14;
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            return pi * radius * radius;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            Console.Write($"Circle.area: {circle.GetArea()}");
+
+
+
+
+    }
+    }
+}
+
+
